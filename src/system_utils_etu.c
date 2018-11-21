ae8e384e37d2047b2f97e07bc34ebe49d8b1a944
Jiben22 <benji.legallo22@live.fr>
2018-11-21 13:55:42 +0100

    Replace system with exec for deleting file + add projectV1 (copy of projectV0)

--- src/system_utils_etu.c
+++ src/system_utils_etu.c
@@ -1,24 +1,31 @@
 /**
- * @file 
+ * @file
  * @brief Implementation by students of usefull function for the system project.
- * @todo Change the SU_removeFile to use exec instead of system.
  */
-
 
 #include "system_utils.h"
 
 /**
  * @brief Maximum length (in character) for a command line.
  **/
-#define SU_MAXLINESIZE (1024*8) 
+#define SU_MAXLINESIZE (1024*8)
 
 /********************** File managment ************/
 
 void SU_removeFile(const char * file){
-  /* VERY Dirty tmp version */
-  char buffer[SU_MAXLINESIZE];
-  snprintf(buffer, SU_MAXLINESIZE, "rm %s",file);
-  //fprintf(stderr, "%s\n", buffer);
-  int resSys = system(buffer);
-  assert(resSys != -1); // TODO : change with err
+	char buffer[SU_MAXLINESIZE];
+	snprintf(buffer, SU_MAXLINESIZE, "%s",file);
+	//fprintf(stderr, "%s\n", buffer);
+
+	//Define command for deleting file
+	pid_t pid = fork();
+	
+	if( pid == -1 )
+		perror("fork");
+	else if( pid == 0)
+	{
+		char *command = "/bin/rm";
+		execl(command, "rm", buffer, NULL );
+	} else
+		wait(NULL);
 }
