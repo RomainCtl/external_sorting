8a7ba7fe89208d4b7da924f02566b9e0cd5cdb12
RomainCtl <romain.chantrel35@gmail.com>
2019-01-28 10:58:52 +0100

    Init Projet V3

--- src/project.c
+++ src/project.c
@@ -9,6 +9,7 @@
 #include "project_v0.h"
 #include "project_v1.h"
 #include "project_v2.h"
+#include "project_v3.h"
 
 /**
  * @brief Maximum length (in character) for a file name.
@@ -78,6 +79,7 @@ int main(int argc, char ** argv){
   int mode_projectV0     = 0;
   int mode_projectV1     = 0;
   int mode_projectV2     = 0;
+  int mode_projectV3     = 0;
   int mode_demoSortSplit = 0;
   int mode_lineCount     = 0;
   int mode_generation    = 0;
@@ -156,6 +158,13 @@ int main(int argc, char ** argv){
       fprintf(stderr, "Missing parameters: need an output and an input file.\n");
       errflg++;
     }
+  } else if(strcmp(mode, "projectV3") == 0){
+    mode_projectV3 = 1;
+    /* need at least in and out file */
+    if(strcmp(i_file, "") == 0 || strcmp(o_file, "") == 0){
+      fprintf(stderr, "Missing parameters: need an output and an input file.\n");
+      errflg++;
+    }
   } else if(strcmp(mode, "test") == 0) {
     mode_test = 1;
   } else if(strcmp(mode, "generation") == 0) {
@@ -219,6 +228,9 @@ int main(int argc, char ** argv){
   } else if(mode_projectV2){
     /* Mode demo sort split*/
     projectV2(i_file, o_file, nb_split);
+  } else if(mode_projectV3){
+    /* Mode demo sort split*/
+    projectV3(i_file, o_file, nb_split);
   }
 
   /* Display end time */
